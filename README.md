2273fdbb24fffb8c49c93b7c5231ab83e102fca7
Suri <suri@Suris-MacBook-Pro-2.local>
2024-08-02 08:42:11 -0700

    adding docstring and update readme

--- README.md
+++ README.md
@@ -44,6 +44,7 @@ This project parses flow log data and maps each row to a tag based on a lookup t
    PYTHONPATH=./src python -m flowlog.parser flowlog_data.txt tag_lookup.csv
    
    NOTE: you can replace your flow log data and lookup. It will generate output file with tag and combination counts.
+   You dont need any dependencies to run the above command, it requires only active virtual environment. Dependencies are only for test, lint and type hit checks.
 
 
 ## Code coverage report
--- src/flowlog/parser.py
+++ src/flowlog/parser.py
@@ -14,7 +14,30 @@ logging.basicConfig(
 
 def load_tag_lookup(filename: str) -> Dict[Tuple[int, str], str]:
     """
-    Loads the tag from lookup CSV.
+    Loads the tag lookup information from a CSV file.
+
+    The CSV file is expected to have three columns:
+                        'dstport', 'protocol', and 'tag'.
+    The function reads the CSV file, skips the header, 
+    and maps each (dstport, protocol) pair to the corresponding tag.
+
+    Args:
+        filename (str):
+            The path to the CSV file containing the tag lookup data.
+
+    Returns:
+        Dict[Tuple[int, str], str]:
+            A dictionary mapping (dstport, protocol) tuples to tags.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        Exception: If there is an error reading the file or
+        processing its contents.
+
+    Logs:
+        INFO: When the tag mappings are successfully loaded.
+        ERROR: If there is an issue loading the lookup file or
+        processing its contents.
     """
     lookup = {}
     try:
@@ -35,6 +58,30 @@ def load_tag_lookup(filename: str) -> Dict[Tuple[int, str], str]:
 
 
 def load_protocol_map(filename):
+    """
+    Loads the protocol map from a JSON file.
+
+    The JSON file is expected to contain key-value pairs where the keys are
+    protocol identifiers (as strings) and the values are protocol
+    names (as strings). The functionreads the JSON file and
+    returns the resulting dictionary.
+
+    Args:
+        filename (str): The path to the JSON file containing the protocol map.
+
+    Returns:
+        dict: A dictionary mapping protocol identifiers to protocol names.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        json.JSONDecodeError: If there is an error decoding the JSON file.
+        Exception: If there is an error reading the file or
+        processing its contents.
+
+    Example:
+        >>> load_protocol_map('protocol_map.json')
+        {'6': 'tcp', '17': 'udp'}
+    """
     with open(filename, 'r') as f:
         return json.load(f)
 
@@ -45,7 +92,38 @@ def parse_flow_logs(
         protocol_map: dict) -> Tuple[Dict[str, int],
                                      Dict[Tuple[int, str], int]]:
     """
-    Parses the flow log from the data file.
+    Parses the flow logs from the given file and generates counts for tags and 
+    port/protocol combinations.
+
+    The function reads the flow log file line by line, extracts
+    the destination port and protocol, and uses the provided lookup
+    and protocol map to categorize and count the occurrences of each tag
+    and port/protocol combination.
+
+    Args:
+        filename (str): The path to the flow log file.
+        lookup (Dict[Tuple[int, str], str]):
+            A dictionary mapping (dstport, protocol) tuples to tags.
+
+        protocol_map (dict): 
+            A dictionary mapping protocol identifiers to protocol names.
+
+    Returns:
+        Tuple[Dict[str, int], Dict[Tuple[int, str], int]]:
+            - A dictionary mapping tags to their counts.
+            - A dictionary mapping (dstport, protocol) tuples to their counts.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        Exception: If there is an error reading the file or
+        processing its contents.
+
+    Example:
+        >>> lookup = {(443, 'tcp'): 'sv_P1', (68, 'udp'): 'sv_P2'}
+        >>> protocol_map = {'6': 'tcp', '17': 'udp'}
+        >>> parse_flow_logs('flowlog_data.txt', lookup, protocol_map)
+        ({'sv_P1': 10, 'sv_P2': 5, 'Untagged': 3},
+         {(443, 'tcp'): 10, (68, 'udp'): 5, (80, 'tcp'): 3})
     """
     tag_counts: DefaultDict[str, int] = defaultdict(int)
     combination_counts: DefaultDict[Tuple[int, str], int] = defaultdict(int)
@@ -73,6 +151,37 @@ def parse_flow_logs(
 
 def generate_output(tag_counts: Dict[str, int],
                     combination_counts: Dict[Tuple[int, str], int]) -> None:
+    """
+    Generates an output file containing counts of tags and
+    port/protocol combinations.
+
+    This function writes the provided tag counts and
+    port/protocol combination counts to an output file named `output.txt`.
+    The output file contains two sections:
+    1. **Tag Counts**:
+        Lists each tag and its corresponding count.
+    2. **Port/Protocol Combination Counts**:
+        Lists each port/protocol combination and its corresponding count.
+
+    Args:
+        tag_counts (Dict[str, int]):
+            A dictionary where keys are tags and values
+            are the counts of each tag.
+        combination_counts (Dict[Tuple[int, str], int]):
+            A dictionary where keys are tuples of (port, protocol) and values
+            are the counts of each port/protocol combination.
+
+    Raises:
+        Exception: If there is an error writing to the output file.
+
+    Example:
+        >>> tag_counts = {'sv_P1': 10, 'sv_P2': 5, 'Untagged': 3}
+        >>> combination_counts = {(443, 'tcp'): 10, (68, 'udp'): 5,
+             (80, 'tcp'): 3}
+        >>> generate_output(tag_counts, combination_counts)
+        # This will create an output.txt file with the specified tag
+        # and combination counts.
+    """
     try:
         with open('output.txt', 'w') as file:
             file.write("Tag Counts:\n\n")
